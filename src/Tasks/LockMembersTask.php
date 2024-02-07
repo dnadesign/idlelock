0cc14080a0a1634ac0519dececbf08985f82b57f
Jules <jules0x@users.noreply.github.com>
2024-02-07 13:40:13 +1300

    Update LockMembersTask.php

--- src/Tasks/LockMembersTask.php
+++ src/Tasks/LockMembersTask.php
@@ -11,16 +11,16 @@ class LockMembersTask extends BuildTask
 {
     protected $title = 'Lock Members Task';
 
-    protected $description = 'Locks member accounts based on last login time. Threshold determined per security Group.';
+    protected $description = 'Locks member accounts based on last login time. Lockout threshold determined per security Group.';
 
     public function run($request)
     {
-        Injector::inst()->get(LoggerInterface::class)->info('Check for idled member accounts...');
+        Injector::inst()->get(LoggerInterface::class)->info('Check for idle member accounts...');
 
         foreach (Member::get() as $member) {
             if ($member->shouldBeLockedOut()) {
                 $member->doLockOutAfterIdle();
-                Injector::inst()->get(LoggerInterface::class)->info(sprintf('Member %s (%s) is now locked out of the CMS after not logging in for a while', $member->getTitle(), $member->ID));
+                Injector::inst()->get(LoggerInterface::class)->info(sprintf('Member %s (%s) is now locked out of the CMS after reaching the idle lockout threshold', $member->getTitle(), $member->ID));
             }
         }
 
